f6c6a3085bb87b8747604fb107b104cb0f529c2f
Santisrb <santiago.rojas@uniagustiniana.edu.co>
2026-08-27 15:11:09 +0000

    cambio

---
+++ conexion.js
@@ -0,0 +1,17 @@
+var texto = "yo";
+var numero = 500;
+var boleano = false;
+var lista = [125, 556];
+var objeto = {"mouse": "teclado"};
+
+function lafun(){
+    var texto; 
+    if(parametro > texto){
+        texto = true
+    }else{
+        texto = false
+    }
+    return texto
+}
+
+console.log(lafun(7))
